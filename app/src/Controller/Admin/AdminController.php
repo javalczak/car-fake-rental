34ffa6c50649091a09544b579eb592caf4864c77
javalczak <javalczak@gmail.com>
2025-01-29 19:38:04 +0100

    security + appFixtures

---
+++ app/src/Controller/Admin/AdminController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class AdminController extends AbstractController
+{
+    #[Route('/admin', name: 'admin_home')]
+    public function index(): Response
+    {
+        return new Response('Admin');
+    }
+}
---
+++ app/src/DataFixtures/AppFixtures.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use DateTime;
+use App\Entity\Admin;
+
+class AppFixtures extends Fixture
+{
+    private $username = 'javalczak';
+    private $pass = 'jadwigaPastuch';
+    private $role = ['ROLE_ADMIN'];
+
+    public function __construct(
+        private readonly UserPasswordHasherInterface $hasher,
+    ){}
+
+    public function load(ObjectManager $manager): void
+    {
+        $newUser = new Admin();
+        $newUser -> setUsername($this -> username);
+        $newUser -> setPassword($this -> hasher -> hashPassword($newUser, $this -> pass));
+        $newUser -> setRoles($this -> role);
+        $newUser -> setAddedAt(new DateTime('now'));
+
+        $manager -> persist($newUser);
+        $manager -> flush();
+
+    }
+}
