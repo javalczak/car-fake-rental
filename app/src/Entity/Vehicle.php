614acbd553a9098dfb55539d6e993cd107a24062
javalczak <javalczak@mbp-javalczak.home>
2025-01-29 13:00:38 +0100

    vehicle schema

---
+++ app/src/Entity/Vehicle.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\VehicleRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: VehicleRepository::class)]
+class Vehicle
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $brand = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $type = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $fuel = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $vin = null;
+
+    #[ORM\Column]
+    private ?bool $maintenance = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getBrand(): ?string
+    {
+        return $this->brand;
+    }
+
+    public function setBrand(string $brand): static
+    {
+        $this->brand = $brand;
+
+        return $this;
+    }
+
+    public function getType(): ?string
+    {
+        return $this->type;
+    }
+
+    public function setType(string $type): static
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    public function getFuel(): ?string
+    {
+        return $this->fuel;
+    }
+
+    public function setFuel(string $fuel): static
+    {
+        $this->fuel = $fuel;
+
+        return $this;
+    }
+
+    public function getVin(): ?string
+    {
+        return $this->vin;
+    }
+
+    public function setVin(string $vin): static
+    {
+        $this->vin = $vin;
+
+        return $this;
+    }
+
+    public function isMaintenance(): ?bool
+    {
+        return $this->maintenance;
+    }
+
+    public function setMaintenance(bool $maintenance): static
+    {
+        $this->maintenance = $maintenance;
+
+        return $this;
+    }
+}
